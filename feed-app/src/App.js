bfa0bc519302ba4f10c5200e72a5674ae12f715e
Alex Elmore <aelmore@merkleinc.com>
2022-01-27 13:33:47 -0600

    add header component and styles

--- feed-app/src/App.js
+++ feed-app/src/App.js
@@ -1,9 +1,14 @@
-function App(){
-    return (
-        <div>
-            <h1>Hello from the App Component!</h1>
-        </div>
-    )
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>Title</h1>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
